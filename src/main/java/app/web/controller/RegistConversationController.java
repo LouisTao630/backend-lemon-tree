2aaa8301d2634c140d2382e2bb5863d9c22b532a
Louis Tao <louistao630@sina.com>
2017-04-05 00:26:54 +0800

    apply springMVC restful response

--- src/main/java/app/web/controller/RegistConversationController.java
+++ src/main/java/app/web/controller/RegistConversationController.java
@@ -1,15 +1,38 @@
 package app.web.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import app.web.data.RegistSpokenSentence;
+
 @RestController
+@RequestMapping("/regist")
 public class RegistConversationController {
-	
-	
-	@RequestMapping("/regist/lastConversation")
-	public @ResponseBody String getLastConversation(){
-		return "Hello World!";
+
+	public static final String[] sentences = new String[] { "能留下您的联系方式吗？我想我会联系你的。", "我的朋友，请问我应该如何称呼您？", "让我们约定一个暗号，这样在人群中我一眼就能看见你。" };
+
+	@RequestMapping(value = "/speak", produces = "application/json")
+	public @ResponseBody List<RegistSpokenSentence> getLastConversation() {
+		List<RegistSpokenSentence> speakQueue = new ArrayList<RegistSpokenSentence>();
+		RegistSpokenSentence s = new RegistSpokenSentence();
+		s.setId(0);
+		s.setSentence(sentences[0]);
+		speakQueue.add(s);
+
+		s = new RegistSpokenSentence();
+		s.setId(1);
+		s.setSentence(sentences[1]);
+		speakQueue.add(s);
+
+		s = new RegistSpokenSentence();
+		s.setId(2);
+		s.setSentence(sentences[2]);
+		speakQueue.add(s);
+
+		return speakQueue;
 	}
 }
---
+++ src/main/java/app/web/data/RegistSpokenSentence.java
@@ -0,0 +1,28 @@
+package app.web.data;
+
+/**
+ * @author Louis Tao
+ *
+ */
+public class RegistSpokenSentence {
+
+	private int id;
+
+	private String sentence;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+}
